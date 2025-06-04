1cd8e9ce53ecdacd26cf1211990845f0c0c7b338
Garou2 <amanmahish73@gmail.com>
2025-06-04 21:09:47 +0530

    adding Redux to implement authentication

---
+++ To_do/src/app/features/auth/authSlice.jsx
@@ -0,0 +1,65 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+const auth = getAuth();
+export const signInWithGoogle = createAsyncThunk(
+  "auth/googleSignIn",
+  async (_, { rejectWithValue }) => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      return {
+        uid: result.user.uid,
+        email: result.user.email,
+        name: result.user.displayName,
+      };
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+export const googleLogout = createAsyncThunk("auth/googleLogout", async () => {
+  await signOut(auth);
+});
+
+const initialState = {
+  user: null,
+  isGuest: false,
+  loading: false,
+  error: null,
+};
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setGuestMode: (state) => {
+      state.isGuest = true;
+      state.user = {
+        uid: "local/guestUser",
+      };
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(signInWithGoogle.pending, (state, action) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(signInWithGoogle.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(signInWithGoogle.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+      })
+      .addCase(googleLogout.fulfilled, (state) => {
+        state.user = null;
+      });
+  },
+});
+export default authSlice.reducer;
